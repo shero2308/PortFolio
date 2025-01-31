533cc1d6216fd5c12af922db96815547daa508cb
shero <rakshitkumar682@gmail.com>
2025-01-31 17:36:46 +0530

    chnages

--- portfolio/src/components/Nav/Nav.jsx
+++ portfolio/src/components/Nav/Nav.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import "./Nav.css"
+import {Link} from "react-scroll"
+
 function Nav() {
   return (
-    <div>
+    <nav>
         <h1>PORTFOLIO</h1>
         <ul>
-            <li>Home</li>
+            <Link to="home"><li>Home</li></Link>
             <li>About</li>
             <li>Projects</li>
             <li>Contact</li>
         </ul>
-    </div>
+    </nav>
   )
 }
 
