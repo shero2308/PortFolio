f8d43aa71a486bf70cc32d28325c625f203e47fe
shero <rakshitkumar682@gmail.com>
2025-02-04 15:20:49 +0530

    homescreen

--- portfolio/src/components/Home/Home.jsx
+++ portfolio/src/components/Home/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './Home.css'
+import pc from "../../assets/pc.png"
+import TypingEffect from "react-typing-effect"
 
 function Home() {
   return (
     <div id="home">
+      <div className="lefthome">
+        <div className="homedetails">
+          <div className="line1">I'M</div>
+          <div className="line2">RAKSHIT KUMAR</div>
+          <div className="line3">
+            <TypingEffect text={["WEB DEVELOPER" , "SOFTWARE DEVELOPER"]} speed={300} eraseSpeed={50} eraseDelay={700} cursor='|'/>
+          </div>
+        </div>
+      </div>
+      <div className="righthome">
+        <img src={pc} alt="" />
+      </div>
 
     </div>
   )
